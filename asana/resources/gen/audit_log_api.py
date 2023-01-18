5644f130d763829670c2c7a05d9682b604e0c9f4
jv-asana <jv-asana@users.noreply.github.com>
2023-01-18 23:00:22 +0000

    [AUTO GENERATED]Generated from OpenAPI Generator
    Files changed:
    M	asana/resources/gen/__init__.py
    M	asana/resources/gen/attachments.py
    M	asana/resources/gen/audit_log_api.py
    M	asana/resources/gen/batch_api.py
    M	asana/resources/gen/custom_field_settings.py
    M	asana/resources/gen/custom_fields.py
    M	asana/resources/gen/events.py
    M	asana/resources/gen/goal_relationships.py
    M	asana/resources/gen/goals.py
    M	asana/resources/gen/jobs.py
    M	asana/resources/gen/organization_exports.py
    M	asana/resources/gen/portfolio_memberships.py
    M	asana/resources/gen/portfolios.py
    M	asana/resources/gen/project_briefs.py
    M	asana/resources/gen/project_memberships.py
    M	asana/resources/gen/project_statuses.py
    M	asana/resources/gen/project_templates.py
    M	asana/resources/gen/projects.py
    M	asana/resources/gen/sections.py
    M	asana/resources/gen/status_updates.py
    M	asana/resources/gen/stories.py
    M	asana/resources/gen/tags.py
    M	asana/resources/gen/tasks.py
    M	asana/resources/gen/team_memberships.py
    M	asana/resources/gen/teams.py
    M	asana/resources/gen/time_periods.py
    M	asana/resources/gen/typeahead.py
    M	asana/resources/gen/user_task_lists.py
    M	asana/resources/gen/users.py
    M	asana/resources/gen/webhooks.py
    M	asana/resources/gen/workspace_memberships.py
    M	asana/resources/gen/workspaces.py
    M	samples/attachments_sample.yaml
    A	samples/auditlogapi_sample.yaml
    A	samples/batchapi_sample.yaml
    A	samples/customfields_sample.yaml
    A	samples/customfieldsettings_sample.yaml
    M	samples/events_sample.yaml
    A	samples/goalrelationships_sample.yaml
    M	samples/goals_sample.yaml
    M	samples/jobs_sample.yaml
    A	samples/organizationexports_sample.yaml
    A	samples/portfoliomemberships_sample.yaml
    M	samples/portfolios_sample.yaml
    A	samples/projectbriefs_sample.yaml
    A	samples/projectmemberships_sample.yaml
    M	samples/projects_sample.yaml
    A	samples/projectstatuses_sample.yaml
    A	samples/projecttemplates_sample.yaml
    M	samples/sections_sample.yaml
    A	samples/statusupdates_sample.yaml
    M	samples/stories_sample.yaml
    M	samples/tags_sample.yaml
    M	samples/tasks_sample.yaml
    A	samples/teammemberships_sample.yaml
    M	samples/teams_sample.yaml
    A	samples/timeperiods_sample.yaml
    M	samples/typeahead_sample.yaml
    M	samples/users_sample.yaml
    A	samples/usertasklists_sample.yaml
    M	samples/webhooks_sample.yaml
    A	samples/workspacememberships_sample.yaml
    M	samples/workspaces_sample.yaml

--- asana/resources/gen/audit_log_api.py
+++ asana/resources/gen/audit_log_api.py
@@ -1,9 +1,11 @@
 # coding=utf-8
-class _AuditLogAPI:
+from asana.page_iteratorimport import AuditLogAPIIterator
+
+class AuditLogAPI:
 
     def __init__(self, client=None):
         self.client = client
-
+    
     def get_audit_log_events(self, workspace_gid, params=None, **options):
         """Get audit log events
         :param str workspace_gid: (required) Globally unique identifier for the workspace or organization.
@@ -14,12 +16,20 @@ class _AuditLogAPI:
             - actor_type {str}:  Filter to events with an actor of this type. This only needs to be included if querying for actor types without an ID. If `actor_gid` is included, this should be excluded.
             - actor_gid {str}:  Filter to events triggered by the actor with this ID.
             - resource_gid {str}:  Filter to events with this resource ID.
-        :param **options
-            - offset {str}:  Offset token. An offset to the next page returned by the API. A pagination request will return an offset token, which can be used as an input parameter to the next request. If an offset is not passed in, the API will return the first page of results. 'Note: You can only pass in an offset that was returned to you via a previously paginated request.'
             - limit {int}:  Results per page. The number of objects to return per page. The value must be between 1 and 100.
+            - offset {str}:  Offset token. An offset to the next page returned by the API. A pagination request will return an offset token, which can be used as an input parameter to the next request. If an offset is not passed in, the API will return the first page of results. 'Note: You can only pass in an offset that was returned to you via a previously paginated request.'
+        :param **options
         :return: Object
         """
         if params is None:
             params = {}
         path = "/workspaces/{workspace_gid}/audit_log_events".replace("{workspace_gid}", workspace_gid)
-        return self.client.get_collection(path, params, **options)
+        options = self.client._merge_options(options)
+        iterator_type = options.get('iterator_type')
+        if iterator_type == 'items':
+            return AuditLogAPIIterator(self.client, path, params, options).items()
+        if iterator_type is None:
+            return self.client.get(path, params, **options)
+        raise Exception(f'Unknown value for "iterator_type" option: {iterator_type}')
+
+        
